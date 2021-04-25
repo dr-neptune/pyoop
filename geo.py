744ad14efd67aefee8763e4470e7f0511d82f9fc
dr-neptune <mrose4@worcester.edu>
2021-04-25 18:12:20 -0400

    antipode

--- geo.py
+++ geo.py
@@ -2,15 +2,14 @@ from datetime import datetime
 from pytz import timezone
 from timezonefinder import TimezoneFinder
 from sunnyday import Weather
+from random import uniform
+from folium import Marker
 
-help(Weather)
+class Geopoint(Marker):
+    latitude_range = (-90, 90)
 
-weather2 = Weather(apikey = "26631f0f41b95fb9f5ac0df9a8f43c92", lat = 41.1, lon = -4.1)
-
-print(weather2.next_12h_simplified())
-
-class Geopoint:
     def __init__(self, latitude, longitude):
+        super().__init__(location = [latitude, longitude])
         self.latitude = latitude
         self.longitude = longitude
         self.weather_key = "26631f0f41b95fb9f5ac0df9a8f43c92"
@@ -27,8 +26,6 @@ class Geopoint:
         weather = Weather(apikey = self.weather_key, lat = self.latitude, lon = self.longitude)
         return weather.next_12h_simplified()
 
-
-tokyo = Geopoint(latitude = 35.7, longitude = 139.7)
-print(tokyo.closest_parallel())
-print(tokyo.get_time())
-print(tokyo.get_weather())
+    @classmethod
+    def random(cls):
+        return cls(latitude = uniform(-90, 90), longitude = uniform(-180, 180))
