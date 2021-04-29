bf616438b81b78662cbedc768432412f6e6fcb5a
dr-neptune <mrose4@worcester.edu>
2021-04-28 20:52:56 -0400

    wiki

--- photo_sharer/main.py
+++ photo_sharer/main.py
@@ -1,9 +1,32 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+import wikipedia
+import requests
+
+
+# communicate with frontend.kv
+Builder.load_file("frontend.kv")
 
 class FirstScreen(Screen):
     def search_image(self):
-        pass
+        # remove image in the image bucket
+        self.manager.current_screen.ids.img.source = "files/hot_dog.png"
+        # search image
+        # get user query from text input
+        query = self.manager.current_screen.ids.txt.text
+        print(f"Now searching: {query}\n")
+        
+        # get wikipedia page and the first image link
+        image_link = wikipedia.page(query).images[0]
+        
+        # download the image
+        image_path = "files/image.png"
+        with open(image_path, "wb") as file:
+            file.write(requests.get(image_link).content)
+        
+        # place new image in the image bucket
+        self.manager.current_screen.ids.img.source = image_path
 
 class RootWidget(ScreenManager):
     pass
