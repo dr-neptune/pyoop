522ac5bb5385979e0f78dfedc4ec0521e81c5b5e
dr-neptune <mrose4@worcester.edu>
2021-04-29 19:36:45 -0400

    whoowhoo

--- photo_sharer/filesharer.py
+++ photo_sharer/filesharer.py
@@ -1,3 +1,5 @@
+from filestack import Client
+
 class FileSharer:
     def __init__(self, filepath, api_key = "AViVqp7suSQWWEdrl6hf9z"):
         self.filepath = filepath
--- photo_sharer/main.py
+++ photo_sharer/main.py
@@ -1,37 +1,82 @@
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen 
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+from kivy.core.clipboard import Clipboard
+
 from filesharer import FileSharer
+import webbrowser
 import time
 
 # communicate with frontend.kv
 Builder.load_file("frontend.kv")
 
+
 class CameraScreen(Screen):
     def start(self):
+        """Starts the camera and changes the button text"""
         self.ids.camera.Index = -1
         self.ids.camera.play = True
         self.ids.camera_button.text = "Stop Camera"
         self.ids.camera.texture = self.ids.camera._camera.texture
-        
+
+
     def stop(self):
+        """stops the camera and changes button text"""
         self.ids.camera.play = False
         self.ids.camera_button.text = "Start Camera"
         self.ids.camera.texture = None
-    
+
+
     def capture(self):
-        filename = "files/" + time.strftime("%Y%m%d-%H%M%S") + ".png"
-        self.ids.camera.export_to_png(filename)
+        """
+        Creates a filename with current time.
+        Captures an image and saves it with the given filename
+        """
+        self.filename = "files/" + time.strftime("%Y%m%d-%H%M%S") + ".png"
+        self.ids.camera.export_to_png(self.filename)
         self.manager.current = "image_screen"
+        self.manager.current_screen.ids.img.source = self.filename
+
 
 class ImageScreen(Screen):
-    pass
+    link_message = "Create a link first!"
+
+    def create_link(self):
+        """
+        Accesses the photo filepath, uploads it to the web
+        and inserts the link in the label widget
+        """
+        file_path = App.get_running_app().root.ids.camera_screen.filename
+        file_sharer = FileSharer(file_path)
+        self.url = file_sharer.share()
+        self.ids.link.text = self.url
+
+    def copy_link(self):
+        """
+        copy link to the clipboard
+        """
+        try:
+            Clipboard.copy(self.url)
+        except:
+            self.ids.link.text = self.link_message
+
+    def open_link(self):
+        """
+        Open link with the default browser
+        """
+        try:
+            webbrowser.open(self.url)
+        except:
+            self.ids.link.text = self.link_message
+
 
 class RootWidget(ScreenManager):
     pass
-    
+
+
 class MainApp(App):
     def build(self):
         return RootWidget()
+
 
 MainApp().run()
