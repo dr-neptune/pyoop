4bf2506e71db5da08db6b309114855c4cd16942b
dr-neptune <mrose4@worcester.edu>
2021-04-28 21:08:34 -0400

    yeah

--- photo_sharer/main.py
+++ photo_sharer/main.py
@@ -9,24 +9,24 @@ import requests
 Builder.load_file("frontend.kv")
 
 class FirstScreen(Screen):
-    def search_image(self):
-        # remove image in the image bucket
-        self.manager.current_screen.ids.img.source = "files/hot_dog.png"
-        # search image
+    def get_image_link(self):
         # get user query from text input
         query = self.manager.current_screen.ids.txt.text
         print(f"Now searching: {query}\n")
-        
+
         # get wikipedia page and the first image link
-        image_link = wikipedia.page(query).images[0]
+        return wikipedia.page(query).images[0]
         
+    def download_image(self):
         # download the image
         image_path = "files/image.png"
         with open(image_path, "wb") as file:
-            file.write(requests.get(image_link).content)
+            file.write(requests.get(self.get_image_link()).content)
+        return image_path
         
+    def set_image(self):
         # place new image in the image bucket
-        self.manager.current_screen.ids.img.source = image_path
+        self.manager.current_screen.ids.img.source = self.download_image()
 
 class RootWidget(ScreenManager):
     pass
